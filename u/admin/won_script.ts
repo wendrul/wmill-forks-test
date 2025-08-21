1227dbafad50b92a4274e23e9d829904a1f752ed
admin <admin@windmill.dev>
2025-08-21 13:22:26 +0200

    [WM] Script 'u/admin/won_script' deployed

---
+++ u/admin/won_script.ts
@@ -0,0 +1,16 @@
+//native
+//you can add proxy support using //proxy http(s)://host:port
+
+// native scripts are bun scripts that are executed on native workers and can be parallelized
+// only fetch is allowed, but imports will work as long as they also use only fetch and the standard lib
+
+//import * as wmill from "windmill-client"
+
+export async function main(example_input: number = 3) {
+  // "3" is the default value of example_input, it can be overriden with code or using the UI
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${example_input}`, {
+    headers: { "Content-Type": "application/json" },
+  });
+  return res.json();
+}
+//asdsasdsadasdasd
