50b09acca2ad9d7d1b4d02ec048730edb10f7b31
admin <admin@windmill.dev>
2025-09-11 17:34:15 +0200

    [WM] Script 'u/admin/speedy_script' deployed

--- u/admin/speedy_script.ts
+++ u/admin/speedy_script.ts
@@ -1,6 +1,6 @@
 // there are multiple modes to add as header: //nobundling //native //npm //nodejs
 // https://www.windmill.dev/docs/getting_started/scripts_quickstart/typescript#modes
-
+//////////////////aaaaaI//I
 // import { toWords } from "number-to-words@1"aaaaaaaa
 import * as wmill from "windmill-client"
 
