a5265b9ee75629ece4d10b4f11928224dee653d2
admin <admin@windmill.dev>
2025-11-25 15:50:24 +0100

    Initialize windmill sync repo

--- f/shared/nicer_flow.flow/inline_script_1.inline_script.ts
+++
@@ -1,4 +0,0 @@
-// import * as wmill from "windmill-client"
-
-export async function main() {
-}
--- u/admin/infallible_app.app/inline_script.inline_script.ts
+++
@@ -1,6 +0,0 @@
-// import * as wmill from "windmill-client"
-
-export async function main(x: string) {
-  console.log(`aha! '${x}' is your choice`)
-  return x
-}
