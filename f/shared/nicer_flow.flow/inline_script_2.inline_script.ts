aed23bd918cde25df48eaf7a4c554b7a0dd21a4e
admin <admin@windmill.dev>
2026-03-09 17:58:04 +0100

    Initialize windmill sync repo

--- f/shared/nicer_flow.flow/inline_script_2.inline_script.ts
+++
@@ -1,5 +0,0 @@
-// import * as wmill from "windmill-client"
-
-export async function main() {
-  await new Promise(resolve => setTimeout(resolve, 10000));
-}
--- u/admin/effective_script.ts
+++
@@ -1,3 +0,0 @@
-export async function main() {
-	throw new Error("This action is not supported in the current version of windmill.")
-}
